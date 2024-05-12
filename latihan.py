e36cbd0e1e4c5e86a62a3941cbc58c59a6d41c53
RafiAkmalSyaiban <syaibanrafiakmal@gmail.com>
2024-05-12 07:11:26 +0700

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,15 @@
+print ("halo dek")
+print ("kuliah atau kerja", end=" ")
+print ("pasti kuliah")
+
+a = "semangat kuliahnya"
+print (a,"\n")
+b = "cape ga kuliahnya"
+print (b,"\n")
+c = "mau ketemu ga"
+print (c)
+print ("\n")
+
+x = "ya"
+y = "tidak"
+print (x,"\t",y)
